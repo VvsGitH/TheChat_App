5fb73b80b4563c28aa5d3f2459cfd25c5058d4ef
Vito Paparella Santorsola <vs.paparella@gmail.com>
2021-03-21 10:29:43 +0100

    Rimossi i file inutili e integrato firebase

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,66 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyCr-FYUZWwOEm1KPpk7hWS3oV1-BNEg96Q',
+	authDomain: 'my-chat-app-aa73c.firebaseapp.com',
+	projectId: 'my-chat-app-aa73c',
+	storageBucket: 'my-chat-app-aa73c.appspot.com',
+	messagingSenderId: '422748533719',
+	appId: '1:422748533719:web:2fc9ec870dbeaf4bad9192',
+	measurementId: 'G-ZVBXSF1QPL',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+// Authentication
+export const auth = firebase.auth();
+
+// Database
+export const firestore = firebase.firestore();
+
+// UTILIS -> Google Auth
+const provider = new auth.GoogleAuthProvider();
+provider.addScope('profile');
+provider.addScope('email');
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+// UTILS -> Add new user to db
+export async function addUserToDb(userAuth, additionalData) {
+	if (!userAuth) return;
+
+	const userRef = firestore.doc('users/' + userAuth.uid);
+	const userSnap = await userRef.get();
+
+	if (!userSnap.exists) {
+		const { displayName, email } = userAuth;
+		const createdAt = new Date();
+		await userRef.set({
+			displayName,
+			email,
+			createdAt,
+			...additionalData,
+		});
+	}
+
+	return userRef;
+}
+
+// UTILS -> Add new msg to db
+export async function sendMessage(message) {
+	if (!message) return;
+
+	const msgsRef = firestore.collection('messages/');
+	const msgsSnap = await msgsRef.get();
+
+	if (msgsSnap.exists) {
+		const { sender, content } = message;
+		const sentAt = new Date();
+		await msgsRef.add({
+			sender,
+			content,
+			sentAt,
+		});
+	}
+}
